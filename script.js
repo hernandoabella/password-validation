ba9be32a31654220bfceae3cdff8d34d7606c7fa
Hernando Abella <hernandoabella@gmail.com>
2023-05-31 01:00:47 -0500

    mejoras en el código

--- script.js
+++ script.js
@@ -23,32 +23,62 @@ function validar() {
   document.getElementById("largo").innerText = "Largo: " + input.length;
 
   if (input.length >= 5) {
-    document.getElementById("validar0").style.color = "green";
+    document.getElementById("icon0").classList.remove("far");
+    document.getElementById("icon0").classList.add("fas");
+    document.getElementById("icon0").classList.add("fa-check-circle");
+    document.getElementById("icon0").style.color = "green";
   } else {
-    document.getElementById("validar0").style.color = "red";
+    document.getElementById("icon0").classList.remove("fas");
+    document.getElementById("icon0").classList.add("far");
+    document.getElementById("icon0").classList.add("fa-times-circle");
+    document.getElementById("icon0").style.color = "red";
   }
 
   if (input.length <= 10) {
-    document.getElementById("validar1").style.color = "green";
+    document.getElementById("icon1").classList.remove("far");
+    document.getElementById("icon1").classList.add("fas");
+    document.getElementById("icon1").classList.add("fa-check-circle");
+    document.getElementById("icon1").style.color = "green";
   } else {
-    document.getElementById("validar1").style.color = "red";
+    document.getElementById("icon1").classList.remove("fas");
+    document.getElementById("icon1").classList.add("far");
+    document.getElementById("icon1").classList.add("fa-times-circle");
+    document.getElementById("icon1").style.color = "red";
   }
 
   if (input.match(/[0-9]/i)) {
-    document.getElementById("validar2").style.color = "green";
+    document.getElementById("icon2").classList.remove("far");
+    document.getElementById("icon2").classList.add("fas");
+    document.getElementById("icon2").classList.add("fa-check-circle");
+    document.getElementById("icon2").style.color = "green";
   } else {
-    document.getElementById("validar2").style.color = "red";
+    document.getElementById("icon2").classList.remove("fas");
+    document.getElementById("icon2").classList.add("far");
+    document.getElementById("icon2").classList.add("fa-times-circle");
+    document.getElementById("icon2").style.color = "red";
   }
 
   if (input.match(/[^A-Za-z0-9-' ']/i)) {
-    document.getElementById("validar3").style.color = "green";
+    document.getElementById("icon3").classList.remove("far");
+    document.getElementById("icon3").classList.add("fas");
+    document.getElementById("icon3").classList.add("fa-check-circle");
+    document.getElementById("icon3").style.color = "green";
   } else {
-    document.getElementById("validar3").style.color = "red";
+    document.getElementById("icon3").classList.remove("fas");
+    document.getElementById("icon3").classList.add("far");
+    document.getElementById("icon3").classList.add("fa-times-circle");
+    document.getElementById("icon3").style.color = "red";
   }
 
   if (input.match(/\s/)) {
-    document.getElementById("validar4").style.color = "red";
+    document.getElementById("icon4").classList.remove("fas");
+    document.getElementById("icon4").classList.add("far");
+    document.getElementById("icon4").classList.add("fa-times-circle");
+    document.getElementById("icon4").style.color = "red";
   } else {
-    document.getElementById("validar4").style.color = "green";
+    document.getElementById("icon4").classList.remove("far");
+    document.getElementById("icon4").classList.add("fas");
+    document.getElementById("icon4").classList.add("fa-check-circle");
+    document.getElementById("icon4").style.color = "green";
   }
 }
