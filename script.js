bc56e984d9dec60b21aa715651b4e0127a563909
Hernando Abella <hernandoabella@gmail.com>
2023-05-31 15:17:54 -0500

    mejoras

--- script.js
+++ script.js
@@ -17,68 +17,73 @@ function ver() {
 
 function validar() {
   var input = document.getElementById("password").value;
+  var passwordLength = input.length;
 
   document.getElementById("password").value = input;
+  document.getElementById("largo").innerText =
+    `Largo de la contraseña: ${passwordLength}`;
 
-  document.getElementById("largo").innerText = "Largo: " + input.length;
+  var validarItems = document.querySelectorAll(".validar");
 
-  if (input.length >= 5) {
-    document.getElementById("icon0").classList.remove("far");
-    document.getElementById("icon0").classList.add("fas");
-    document.getElementById("icon0").classList.add("fa-check-circle");
-    document.getElementById("icon0").style.color = "green";
-  } else {
-    document.getElementById("icon0").classList.remove("fas");
-    document.getElementById("icon0").classList.add("far");
-    document.getElementById("icon0").classList.add("fa-times-circle");
-    document.getElementById("icon0").style.color = "red";
-  }
+  validarItems.forEach(function (item, index) {
+    var icon = item.querySelector(".icon");
 
-  if (input.length <= 10) {
-    document.getElementById("icon1").classList.remove("far");
-    document.getElementById("icon1").classList.add("fas");
-    document.getElementById("icon1").classList.add("fa-check-circle");
-    document.getElementById("icon1").style.color = "green";
-  } else {
-    document.getElementById("icon1").classList.remove("fas");
-    document.getElementById("icon1").classList.add("far");
-    document.getElementById("icon1").classList.add("fa-times-circle");
-    document.getElementById("icon1").style.color = "red";
-  }
-
-  if (input.match(/[0-9]/i)) {
-    document.getElementById("icon2").classList.remove("far");
-    document.getElementById("icon2").classList.add("fas");
-    document.getElementById("icon2").classList.add("fa-check-circle");
-    document.getElementById("icon2").style.color = "green";
-  } else {
-    document.getElementById("icon2").classList.remove("fas");
-    document.getElementById("icon2").classList.add("far");
-    document.getElementById("icon2").classList.add("fa-times-circle");
-    document.getElementById("icon2").style.color = "red";
-  }
-
-  if (input.match(/[^A-Za-z0-9-' ']/i)) {
-    document.getElementById("icon3").classList.remove("far");
-    document.getElementById("icon3").classList.add("fas");
-    document.getElementById("icon3").classList.add("fa-check-circle");
-    document.getElementById("icon3").style.color = "green";
-  } else {
-    document.getElementById("icon3").classList.remove("fas");
-    document.getElementById("icon3").classList.add("far");
-    document.getElementById("icon3").classList.add("fa-times-circle");
-    document.getElementById("icon3").style.color = "red";
-  }
-
-  if (input.match(/\s/)) {
-    document.getElementById("icon4").classList.remove("fas");
-    document.getElementById("icon4").classList.add("far");
-    document.getElementById("icon4").classList.add("fa-times-circle");
-    document.getElementById("icon4").style.color = "red";
-  } else {
-    document.getElementById("icon4").classList.remove("far");
-    document.getElementById("icon4").classList.add("fas");
-    document.getElementById("icon4").classList.add("fa-check-circle");
-    document.getElementById("icon4").style.color = "green";
-  }
+    switch (index) {
+      case 0:
+        if (passwordLength >= 5) {
+          item.classList.add("valid");
+          icon.classList.remove("fa-times-circle");
+          icon.classList.add("fa-check-circle");
+        } else {
+          item.classList.remove("valid");
+          icon.classList.remove("fa-check-circle");
+          icon.classList.add("fa-times-circle");
+        }
+        break;
+      case 1:
+        if (passwordLength <= 10) {
+          item.classList.add("valid");
+          icon.classList.remove("fa-times-circle");
+          icon.classList.add("fa-check-circle");
+        } else {
+          item.classList.remove("valid");
+          icon.classList.remove("fa-check-circle");
+          icon.classList.add("fa-times-circle");
+        }
+        break;
+      case 2:
+        if (/\d/.test(input)) {
+          item.classList.add("valid");
+          icon.classList.remove("fa-times-circle");
+          icon.classList.add("fa-check-circle");
+        } else {
+          item.classList.remove("valid");
+          icon.classList.remove("fa-check-circle");
+          icon.classList.add("fa-times-circle");
+        }
+        break;
+      case 3:
+        if (/[^\w\s]/.test(input)) {
+          item.classList.add("valid");
+          icon.classList.remove("fa-times-circle");
+          icon.classList.add("fa-check-circle");
+        } else {
+          item.classList.remove("valid");
+          icon.classList.remove("fa-check-circle");
+          icon.classList.add("fa-times-circle");
+        }
+        break;
+      case 4:
+        if (!/\s/.test(input)) {
+          item.classList.add("valid");
+          icon.classList.remove("fa-times-circle");
+          icon.classList.add("fa-check-circle");
+        } else {
+          item.classList.remove("valid");
+          icon.classList.remove("fa-check-circle");
+          icon.classList.add("fa-times-circle");
+        }
+        break;
+    }
+  });
 }
